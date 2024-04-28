4eb39b0ba5d416f0e9b8b877f8a72ad34448b7fc
DaniyarMashayev <dmashaev@gmail.com>
2024-04-28 23:04:23 +0500

    second step

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     application
     jacoco
     id("checkstyle")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "hexlet.code"
@@ -21,6 +22,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
 }
 
 tasks.test {
