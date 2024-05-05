2ce185e103d2935f3af64497de0f928f553e7dfe
DaniyarMashayev <dmashaev@gmail.com>
2024-05-05 17:05:16 +0500

    fifth step

---
+++ app/src/main/java/hexlet/code/contoller/UrlsController.java
@@ -0,0 +1,43 @@
+package hexlet.code.contoller;
+
+import hexlet.code.dto.BasePage;
+import hexlet.code.model.Url;
+import hexlet.code.repository.UrlsRepository;
+import hexlet.code.util.NamedRoutes;
+import io.javalin.http.Context;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.Objects;
+
+public class UrlsController {
+    public static void create(Context ctx) throws URISyntaxException {
+        var inputUrl = ctx.formParamAsClass("url", String.class).get();
+        URI parsedUrl;
+        try {
+            parsedUrl = new URI(inputUrl);
+            if (Objects.equals(parsedUrl.getScheme(), null) || Objects.equals(parsedUrl.getAuthority(), null)) {
+                throw new URISyntaxException(parsedUrl.toString(), "Некорректный URL");
+            }
+        } catch (URISyntaxException e) {
+            var page = new BasePage("Некорректный URL", "danger");
+            ctx.status(400);
+            ctx.render("index.jte", Collections.singletonMap("page", page));
+        }
+        parsedUrl = new URI(inputUrl);
+        var name = parsedUrl.getScheme() + "://" + parsedUrl.getAuthority();
+        Url newUrl = new Url(name);
+
+        if (UrlsRepository.find(newUrl.getName()).isPresent()) {
+            ctx.sessionAttribute("flash", "Страница уже существует");
+            ctx.sessionAttribute("flashType", "warning");
+        } else {
+            UrlsRepository.save(newUrl);
+            ctx.sessionAttribute("flash", "Страница успешно добавлена");
+            ctx.sessionAttribute("flashType", "success");
+        }
+        ctx.redirect(NamedRoutes.urlsPath());
+    }
+
+}
--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -3,14 +3,23 @@ package hexlet.code.model;
 
 import java.sql.Timestamp;
 
-import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 
+@Getter
 @Setter
-@AllArgsConstructor
+@ToString
 public class Url {
     private Long id;
+
+    @ToString.Include
     private String name;
+
     private Timestamp createdAt;
+
+    public Url(String name) {
+        this.name = name;
+    }
 }
