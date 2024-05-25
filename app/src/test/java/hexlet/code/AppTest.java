0a4e428b9fdcb3dfd1631380795634726f6ff26c
DaniyarMashayev <dmashaev@gmail.com>
2024-05-25 14:14:02 +0500

    update .gitignore and AppTest

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -4,6 +4,7 @@ import static hexlet.code.App.readResourceFile;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -16,6 +17,7 @@ import hexlet.code.repository.UrlCheckRepository;
 import hexlet.code.repository.UrlsRepository;
 import hexlet.code.util.NamedRoutes;
 import io.javalin.Javalin;
+import io.javalin.http.NotFoundResponse;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import org.junit.jupiter.api.BeforeAll;
@@ -50,36 +52,6 @@ public class AppTest {
     }
 
     @Test
-    public void testUrl1() {
-        JavalinTest.test(app, (server, client) -> {
-            var request = "url=https://test-domain.com";
-            var response = client.post(NamedRoutes.urlsPath(), request);
-            assertThat(response.code()).isEqualTo(200);
-            assertThat(response.body().string()).contains("https://test-domain.com");
-            assertThat(UrlsRepository.getEntities()).hasSize(1);
-        });
-    }
-
-    @Test
-    public void testUrl2() {
-        JavalinTest.test(app, (server, client) -> {
-            var request = "url=https://test-domain.com:8080/example/test";
-            var response = client.post(NamedRoutes.urlsPath(), request);
-            assertThat(response.code()).isEqualTo(200);
-            assertThat(response.body().string()).contains("https://test-domain.com:8080");
-            assertThat(UrlsRepository.getEntities()).hasSize(1);
-        });
-    }
-
-    @Test
-    public void testUrlNotFound() {
-        JavalinTest.test(app, (server, client) -> {
-            var response = client.get(NamedRoutes.urlPath(99999L));
-            assertThat(response.code()).isEqualTo(404);
-        });
-    }
-
-    @Test
     public void testUrlsPage() {
         JavalinTest.test(app, (server, client) -> {
             var response = client.get(NamedRoutes.urlsPath());
@@ -103,6 +75,44 @@ public class AppTest {
             assertThat(Objects.requireNonNull(response.body()).string()).contains(input);
             assertEquals(UrlsRepository.find(url.getId()).orElseThrow().getName(), input);
             assertEquals(UrlsRepository.find(input).orElseThrow().getName(), input);
+        });
+    }
+
+    @Test
+    public void testRegisterNewUrl() {
+        String input = "url=https://www.mail.ru";
+        JavalinTest.test(app, (server, client) -> {
+            client.post(NamedRoutes.urlsPath(), input);
+            var response = client.get(NamedRoutes.urlsPath());
+            var bodyString = Objects.requireNonNull(response.body()).string();
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(UrlsRepository.getEntities()).hasSize(1);
+            Url url = UrlsRepository.find("https://www.mail.ru")
+                    .orElseThrow(() -> new NotFoundResponse("Url = https://www.mail.ru not found"));
+            assertThat(bodyString).contains("https://www.mail.ru");
+            assertFalse(url.toString().isEmpty());
+            assertEquals("https://www.mail.ru", url.getName());
+        });
+    }
+
+
+
+    @Test
+    public void testDoubleUrlPage() {
+        String input = "url=https://www.mail.ru";
+
+        JavalinTest.test(app, (server, client) -> {
+            client.post(NamedRoutes.urlsPath(), input);
+            client.post(NamedRoutes.urlsPath(), input);
+            assertThat(UrlsRepository.getEntities()).hasSize(1);
+        });
+    }
+
+    @Test
+    public void testUrlNotFound() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get(NamedRoutes.urlPath(99999L));
+            assertThat(response.code()).isEqualTo(404);
         });
     }
 
